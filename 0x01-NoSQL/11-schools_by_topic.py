5735beed75fc9ef7cebab39385d612dabe8ec13a
Shaker Sharabi <sheko100@icloud.com>
2024-06-18 20:37:34 +0300

    Add 11-schools_by_topic.py file

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""Module that defines schools_by_topic function
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Selects school by a topic"""
+
+    docs = []
+
+    coll_docs = mongo_collection.find()
+
+    for doc in coll_docs:
+        print(doc)
+        if 'topics' in doc.keys() and topic in doc['topics']:
+            docs.append(doc)
+
+    return docs
