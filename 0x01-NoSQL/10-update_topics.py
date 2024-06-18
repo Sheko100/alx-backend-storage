604f29b43219b08bf6ddef9dab17393c50855ecf
Shaker Sharabi <sheko100@icloud.com>
2024-06-18 20:17:52 +0300

    Add 10-update_topics.py file

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""Module that defines update_topics function
+"""
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates topics a school document based on the name"""
+
+    update = mongo_collection.update_one(
+            {'name': name},
+            {'$set': {'topics': topics}}
+            )
