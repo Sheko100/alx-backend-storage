698094415588e611f39ca9504660556ffb69a0a8
Shaker Sharabi <sheko100@icloud.com>
2024-06-18 19:39:20 +0300

    Add 8-all.py file

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Module that defines list_all function
+"""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """Lists all documents in mongo_collection"""
+    docs = []
+
+    coll_docs = mongo_collection.find()
+
+    for doc in coll_docs:
+        docs.append(doc)
+
+    return docs
