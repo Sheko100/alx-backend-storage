9580fe5824e2b2768891bf9aa1a4ecea22e4999c
Shaker Sharabi <sheko100@icloud.com>
2024-06-18 19:56:48 +0300

    Add 9-insert_school.py file

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Module that defines insert_school function
+"""
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts documents in the mongo_collection"""
+
+    new_doc = mongo_collection.insert_one(kwargs)
+
+    return (new_doc.inserted_id)
