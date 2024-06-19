bb2cc4611b5362574d418817398dd3ef446403d8
Shaker Sharabi <sheko100@icloud.com>
2024-06-19 19:16:52 +0300

    Add exercise.py file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module that defines the Cache class
 """
 import redis
-from typing import Union
+from typing import Union, Callable, List
 from uuid import uuid4
 
 
@@ -26,3 +26,47 @@ class Cache:
         self._redis.set(new_key, data)
 
         return new_key
+
+    def get(self, key: str, fn: Callable = None) -> Union[
+            str, bytes, int, float, List]:
+        """Retrieve a value based on key and converts its type using fn
+
+        Args:
+            key: key to retreive the value
+            fn: Callabe to convert the retrieved value
+
+        Returns:
+            value: the retrieved value
+        """
+        value = self._redis.get(key)
+
+        if value and fn:
+            value = fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Retrieves a string value based on the key
+
+        Args:
+            key: key associted with the value
+
+        Returns:
+            value = the string value
+        """
+        value = self.get(key, str)
+
+        return value
+
+    def get_int(self, key: str) -> int:
+        """Retrieves an integer value based on the key
+
+        Args:
+            key: key associted with the value
+
+        Returns:
+            value = the integer value
+        """
+        value = self.get(key, int)
+
+        return value
